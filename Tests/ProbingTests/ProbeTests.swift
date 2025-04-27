a687571641fe177149c3a1e5cb94e47eebed597f
Kamil Strzelecki <strzeleckikamil.developer@gmail.com>
2025-04-27 10:04:48 +0200

    -

---
+++ Tests/ProbingTests/ProbeTests.swift
@@ -0,0 +1,34 @@
+//
+//  ProbeTests.swift
+//  Probing
+//
+//  Created by Kamil Strzelecki on 27/04/2025.
+//  Copyright © 2025 Kamil Strzelecki. All rights reserved.
+//
+
+@testable import Probing
+import Testing
+
+internal struct ProbeTests {
+
+    @Test
+    func testProbe() async {
+        await #probe()
+    }
+
+    @Test
+    func testProbeInTask() async {
+        let task = Task {
+            await #probe()
+        }
+        await task.value
+    }
+
+    @Test
+    func testProbeInEffect() async {
+        let effect = #Effect("test") {
+            await #probe()
+        }
+        await effect.value
+    }
+}
